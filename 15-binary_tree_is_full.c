32ef6ac563a56d07497cb2774f067dce3d2abc27
GitGolik <goliktheo.pro@gmail.com>
2026-08-04 14:39:41 +0200

    15-binary_tree_is_full.c

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - check if the tree is full
+ * @tree: pointer to the root node of the tree checked
+ * Return: 1 if full , 0 if not
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if ((tree->left == NULL) && (tree->right == NULL))
+	{
+		return (1);
+	}
+
+	if ((tree->left == NULL) || (tree->right == NULL))
+	{
+		return (0);
+	}
+
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+}
