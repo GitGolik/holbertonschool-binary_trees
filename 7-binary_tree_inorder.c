3a0ad3eee4f76c1da24119945f952cc66f244566
GitGolik <goliktheo.pro@gmail.com>
2026-08-04 11:16:22 +0200

    7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,7 +6,7 @@
  * @func: pointer to the functions call of each node
  * Return: no return
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if ((tree == NULL) || (func == NULL))
 	{
