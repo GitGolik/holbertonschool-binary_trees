fb271d23a0b5cc95a174be9b85a6fe4bae78317e
GitGolik <goliktheo.pro@gmail.com>
2026-08-04 14:58:06 +0200

    17-binary_tree_sibling.c

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - search the sibling of a node
+ * @node: pointer to the node to find the sibling
+ * Return: pointer to the sibling node or NULL if no sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *parent;
+
+	if ((node == NULL) || (node->parent == NULL))
+		return (NULL);
+
+	parent = node->parent;
+
+	if (parent->left == node)
+		return (parent->right);
+
+	if (parent->right == node)
+		return (parent->left);
+
+	return (NULL);
+}
